2fb879eb0ff09a374c8419633ffdb352c1347f6a
royklip <royklip2@gmail.com>
2018-04-02 10:57:09 +0200

    Add more info, timer, topics and questions

--- oTree/time_pressure/models.py
+++ oTree/time_pressure/models.py
@@ -2,12 +2,12 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
+import random
 
-
-author = 'Your name here'
+author = 'Boris Mattijssen & Roy Klip'
 
 doc = """
-Your app description
+Information Retrieval
 """
 
 
@@ -15,10 +15,28 @@ class Constants(BaseConstants):
     name_in_url = 'time_pressure'
     players_per_group = None
     num_rounds = 1
+    choices = [
+        [1, ''],
+        [2, ''],
+        [3, ''],
+        [4, ''],
+        [5, ''],
+        [6, ''],
+        [7, ''],
+    ]
+    topics = [
+        'topic 1',
+        'topic 2',
+        'topic 3',
+        'topic 4'
+    ]
 
 
 class Subsession(BaseSubsession):
-    pass
+    def creating_session(self):
+        for p in self.get_players():
+            p.topic = Constants.topics[(p.id_in_group % 4) - 1]
+            p.treatment = random.choice([True, False])
 
 
 class Group(BaseGroup):
@@ -26,4 +44,23 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    feel_time_pressure = models.IntegerField(
+        label='Did you feel time pressure?',
+        choices=Constants.choices,
+        widget=widgets.RadioSelectHorizontal
+    )
+    need_work_fast = models.IntegerField(
+        label='Did you feel the need to work fast?',
+        choices=Constants.choices,
+        widget=widgets.RadioSelectHorizontal
+    )
+    feel_hurried_rushed = models.IntegerField(
+        label='Did you feel hurried or rushed?',
+        choices=Constants.choices,
+        widget=widgets.RadioSelectHorizontal
+    )
+
     total_task_time = models.FloatField()
+
+    topic = models.StringField()
+    treatment = models.BooleanField()
--- oTree/time_pressure/pages.py
+++ oTree/time_pressure/pages.py
@@ -8,21 +8,23 @@ class Introduction(Page):
 
 
 class Search(Page):
+    def get_timeout_seconds(self):
+        if self.player.treatment == True:
+            return 300
+        else:
+            return 900
+
     form_model = 'player'
-    form_fields = ['total_task_time']
+    form_fields = ['topic', 'total_task_time']
 
 
 class Questionnaire(Page):
-    pass
-
-
-class End(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['feel_time_pressure', 'need_work_fast', 'feel_hurried_rushed']
 
 
 page_sequence = [
     Introduction,
     Search,
-    Questionnaire,
-    End
+    Questionnaire
 ]
